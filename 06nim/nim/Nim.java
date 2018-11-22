42a5fb05ade1486dc648a06df8a94d2797304744
Jan Janech <jan.janech@fri.uniza.sk>
2018-11-22 12:52:19 +0100

    meno hraca na tahu

--- 06nim/nim/Nim.java
+++ 06nim/nim/Nim.java
@@ -8,11 +8,19 @@
 public class Nim {
     private Sachovnica sachovnica;
     private Kamen kamen;
+    private String[] menaHracov;
+    private int poradoveCisloHraca;
     
-    public Nim(int sirka, int vyska) {
+    public Nim(int sirka, int vyska, String menoPrveho, String menoDruheho) {
         this.sachovnica = new Sachovnica(sirka, vyska);
         this.kamen = this.sachovnica.pridajKamen();
         this.sachovnica.zobraz();
+        this.menaHracov = new String[] { menoPrveho, menoDruheho };
+        this.poradoveCisloHraca = 0;
+    }
+    
+    public String getMenoHracaNaTahu() {
+        return this.menaHracov[this.poradoveCisloHraca];
     }
     
     public void tahDole(int pocet) {
@@ -24,6 +32,8 @@ public class Nim {
         int novyStlpec = this.kamen.getAktualnyStlpec();
         
         this.kamen.posunNa(novyStlpec, novyRiadok);
+        
+        this.poradoveCisloHraca = (this.poradoveCisloHraca + 1) % this.menaHracov.length;
     }
     
     public void tahVlavo(int pocet) {
@@ -35,5 +45,7 @@ public class Nim {
         int novyStlpec = this.kamen.getAktualnyStlpec() - pocet;
         
         this.kamen.posunNa(novyStlpec, novyRiadok);
+        
+        this.poradoveCisloHraca = (this.poradoveCisloHraca + 1) % this.menaHracov.length;
     }
 }
